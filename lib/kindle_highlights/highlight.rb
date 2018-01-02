0924a39403b5c682c43fcda9c7629f4be3f38d82
andykemp <spam@1kemp.co.uk>
2018-01-02 22:19:36 +0000

    Add Page Number when available
    Adds a attribute which records the page number/location depending on what is available for the particular book it returns:
    'Page: 57' or 'Location: 1277' as appropriate.

--- lib/kindle_highlights/highlight.rb
+++ lib/kindle_highlights/highlight.rb
@@ -1,18 +1,20 @@
 module KindleHighlights
   class Highlight
-    attr_accessor :asin, :text, :location
+    attr_accessor :asin, :text, :page, :location
 
     def self.from_html_elements(book:, html_elements:)
       new(
         asin: book.asin,
         text: html_elements.children.search("div.kp-notebook-highlight").first.text.squish,
-        location: html_elements.children.search("input#kp-annotation-location").first.attributes["value"].value,
+        page: html_elements.children.search("span#annotationHighlightHeader").first.text.partition('|').last.lstrip.chop,
+		location: html_elements.children.search("input#kp-annotation-location").first.attributes["value"].value,
       )
     end
 
-    def initialize(asin:, text:, location:)
+    def initialize(asin:, text:, page:, location:)
       @asin = asin
       @text = text
+	  @page = page
       @location = location
     end
 
